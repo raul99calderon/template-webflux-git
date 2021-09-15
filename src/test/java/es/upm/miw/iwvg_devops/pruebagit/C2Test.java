65c0b6dc6be19edeb583574b27301f2c3cd2572e
Raúl Calderón <raul.calderon@alumnos.upm.es>
2021-09-15 13:46:49 +0200

    C2 tests

---
+++ src/test/java/es/upm/miw/iwvg_devops/pruebagit/C2Test.java
@@ -0,0 +1,18 @@
+package es.upm.miw.iwvg_devops.pruebagit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class C2Test {
+
+    @Test
+    void getHelloWorld() {
+        assertEquals(new C2().getHelloWorld(),"HelloWorld");
+    }
+
+    @Test
+    void getNum() {
+        assertEquals(new C2().getNum(),2);
+    }
+}
