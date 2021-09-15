bf31b3f967870f315a7f03a60bd94eea52f3d4ac
Raúl Calderón <raul.calderon@alumnos.upm.es>
2021-09-15 12:42:46 +0200

    updated BadgeTest.java: v2.2 -> v2.3

--- src/test/java/es/upm/miw/iwvg_devops/rest/BadgeTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/BadgeTest.java
@@ -9,7 +9,7 @@ class BadgeTest {
 
     @Test
     void testGenerateBadge() {
-        String badge = new Badge().generateBadge("Heroku", "v2.2.0-SNAPSHOT");
+        String badge = new Badge().generateBadge("Heroku", "v2.3.0-SNAPSHOT");
         assertNotNull(badge);
         assertEquals("<svg", badge.substring(0, 4));
     }
